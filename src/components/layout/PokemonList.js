169796519f617194c55f8a62f7c54314ad918f64
Dusernajder <dusungulu.tibi@gmail.com>
2020-11-07 13:32:01 +0100

    pokemon details state

--- src/components/layout/PokemonList.js
+++ src/components/layout/PokemonList.js
@@ -4,7 +4,11 @@ import Pokemon from "../Pokemon";
 class PokemonList extends Component {
   render() {
     return this.props.pokemons.map((pokemon) => (
-      <Pokemon key={pokemon.name} pokemon={pokemon} />
+      <Pokemon
+        key={pokemon.name}
+        pokemon={pokemon}
+        detailPokemon={this.props.detailPokemon.bind(this, pokemon)}
+      />
     ));
   }
 }
