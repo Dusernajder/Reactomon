d78c93823d6f490e909bf342c541948a7c3497e9
Dusernajder <dusungulu.tibi@gmail.com>
2020-11-07 13:26:42 +0100

    fetching limit increased from 20 to 40

--- src/App.js
+++ src/App.js
@@ -16,9 +16,11 @@ class App extends Component {
   }
 
   componentDidMount() {
-    axios
-      .get("https://pokeapi.co/api/v2/pokemon")
-      .then((response) => this.setState({ pokemons: response.data.results }));
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=40").then((response) =>
+      this.setState({
+        pokemons: response.data.results,
+      })
+    );
   }
 
   render() {
