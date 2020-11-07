721dbe803cc195c4abd18cb12aa05886ee9e229e
Dusernajder <dusungulu.tibi@gmail.com>
2020-11-07 13:30:01 +0100

    styling for details page

--- src/components/layout/PokemonDetail.js
+++ src/components/layout/PokemonDetail.js
@@ -23,15 +23,19 @@ class PokemonDetail extends Component {
 
   render() {
     return (
-      <div>
-        <div>
+      <div style={pokemonCard}>
+        <div style={flexboxStyle}>
           <div>
-            <img src={this.state.pokemon.img} alt={this.state.pokemon.name} />
+            <img
+              src={this.state.pokemon.img}
+              alt={this.state.pokemon.name}
+              style={imgStyle}
+            />
           </div>
-          <div>
+          <div style={mainInfo}>
             <p>{this.state.pokemon.name}</p>
           </div>
-          <div>
+          <div style={detailsInfo}>
             <div>
               <p>Height: {this.state.pokemon.height}</p>
               <p>Weight: {this.state.pokemon.weight}</p>
@@ -42,5 +46,40 @@ class PokemonDetail extends Component {
     );
   }
 }
+
+const pokemonCard = {
+  boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+  borderRadius: "0px 0px 10px 10px",
+  background: "#282c34",
+};
+
+const flexboxStyle = {
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  justifyContent: "space-around",
+};
+
+const imgStyle = {
+  maxWidth: "200px",
+  margin: "10px",
+};
+
+const mainInfo = {
+  color: "rgb(248, 204, 70)",
+  textAlign: "center",
+  fontSize: "30px",
+};
+
+const detailsInfo = {
+  textAlign: "left",
+  color: "#282c34",
+  background: "rgb(248, 204, 70)",
+  margin: "5px",
+  borderRadius: "5px",
+  minWidth: "200px",
+  textAlign: "center",
+  boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+};
 
 export default PokemonDetail;
