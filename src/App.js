f98b4651077d06624ba92472f5b7de5161f136f8
Dusernajder <dusungulu.tibi@gmail.com>
2020-11-08 20:07:20 +0100

    convert component to functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import PokemonList from "./components/layout/PokemonList";
 import PokemonDetail from "./components/layout/PokemonDetail";
@@ -7,47 +7,42 @@ import TypeList from "./components/layout/TypeList";
 import Navbar from "./components/layout/Navbar";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      pokemons: [],
-      pokemon: "",
-    };
-  }
+const App = (props) => {
+  const [pokemons, setPokemons] = useState([]);
+  const [pokemon, setPokemon] = useState({});
 
-  componentDidMount() {
-    axios.get("https://pokeapi.co/api/v2/pokemon?limit=40").then((response) =>
-      this.setState({
-        pokemons: response.data.results,
-      })
-    );
-  }
+  const fetchPokemons = () => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=40").then((response) => {
+      setPokemons(response.data.results);
+    });
+  };
 
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route path="/(pokemons|)">
-            <div style={pokemonContainer}>
-              <PokemonList
-                pokemons={this.state.pokemons}
-                detailPokemon={(pokemon) => this.setState({ pokemon: pokemon })}
-              />
-            </div>
-          </Route>
-          <Route path="/pokemon/:id">
-            <PokemonDetail pokemon={this.state.pokemon} />
-          </Route>
-          <Route path="/types">
-            <TypeList />
-          </Route>
-        </div>
-      </Router>
-    );
-  }
-}
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route path="/(pokemons|)">
+          <div style={pokemonContainer}>
+            <PokemonList
+              pokemons={pokemons}
+              detailPokemon={(pokemon) => setPokemon(pokemon)}
+            />
+          </div>
+        </Route>
+        <Route path="/pokemon/:id">
+          <PokemonDetail pokemon={pokemon} />
+        </Route>
+        <Route path="/types">
+          <TypeList />
+        </Route>
+      </div>
+    </Router>
+  );
+};
 
 const pokemonContainer = {
   display: "flex",
