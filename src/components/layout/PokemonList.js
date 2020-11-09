77d45307f07953cd56745c73b6abd1e80218ae4a
Dusernajder <dusungulu.tibi@gmail.com>
2020-11-09 03:08:46 +0100

    convert component to functional component

--- src/components/layout/PokemonList.js
+++ src/components/layout/PokemonList.js
@@ -1,16 +1,14 @@
-import { Component } from "react";
+import React from "react";
 import Pokemon from "../Pokemon";
 
-class PokemonList extends Component {
-  render() {
-    return this.props.pokemons.map((pokemon) => (
-      <Pokemon
-        key={pokemon.name}
-        pokemon={pokemon}
-        detailPokemon={this.props.detailPokemon.bind(this, pokemon)}
-      />
-    ));
-  }
-}
+const PokemonList = (props) => {
+  return props.pokemons.map((pokemon) => (
+    <Pokemon
+      key={pokemon.name}
+      pokemon={pokemon}
+      detailPokemon={props.detailPokemon.bind(this, pokemon)}
+    />
+  ));
+};
 
 export default PokemonList;
